517f83a5adb4afa17dbdae2010efe1343e5da4e1
KySch <73161771+KySch@users.noreply.github.com>
2021-01-04 19:44:13 +0100

    StartedAssignment
    Did Task 1 A,B,D,E,A,G

---
+++ 2DCarGame/Assets/Scripts/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] float racingCarSpeed = 0.02f;
+    Material myMaterial;
+    Vector2 offSet;
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, racingCarSpeed);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+
+}
+
---
+++ 2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    float xMin, xMax, yMin, yMax;
+
+    [SerializeField] float padding = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ViewPortToWorldPoint();
+    }
+
+    private void ViewPortToWorldPoint()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
